684e7f74bb88336d513bb6606f2d5790477f2627
CLAIRE-LHV <claire.lhv@gmail.com>
2021-06-01 21:00:21 +1000

    using prettier to format codes

--- pet-the-cat.js
+++ pet-the-cat.js
@@ -1,12 +1,17 @@
-function petcat(){  document.getElementById("DoNotPet").className = "hidden";  document.getElementById("GotBitten").className = "";
- var mess = "You've been biten by the cat!";
- lateAlert(mess);
+function petcat() {
+  document.getElementById('DoNotPet').className = 'hidden';
+  document.getElementById('GotBitten').className = '';
+  var mess = "You've been biten by the cat!";
+  lateAlert(mess);
 }
 
-function ouch(){  document.getElementById("DoNotPet").className = "";  document.getElementById("GotBitten").className = "hidden"; 
+function ouch() {
+  document.getElementById('DoNotPet').className = '';
+  document.getElementById('GotBitten').className = 'hidden';
 }
 
 function lateAlert(message) {
-  setTimeout(function(){ alert(message); }, 500);
+  setTimeout(function () {
+    alert(message);
+  }, 500);
 }
-
