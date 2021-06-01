cc992884bee342783e50b747a874d1fb7ee5d76d
CLAIRE-LHV <claire.lhv@gmail.com>
2021-06-01 22:20:06 +1000

    fixing bitten

--- pet-the-cat.js
+++ pet-the-cat.js
@@ -1,7 +1,7 @@
 function petcat() {
   document.getElementById('DoNotPet').className = 'hidden';
   document.getElementById('GotBitten').className = 'bite container';
-  var mess = "You've been biten by the cat!";
+  var mess = "You've been bitten by the cat!";
   lateAlert(mess);
 }
 
